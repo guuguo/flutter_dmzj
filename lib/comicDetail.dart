c1c5f06243cb64f3ff2cd09adebe5e6f66301bc0
guuguo <guuguo@qq.com>
2018-01-25 16:34:29 +0800

    详情页进行中

--- lib/comicDetail.dart
+++ lib/comicDetail.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_demo/api.dart';
 import 'package:flutter_demo/api.dart';
@@ -101,99 +102,170 @@ class _ComicDetaiPageState extends State<ComicDetailPage>
   }
 
   Widget buildBody(BuildContext context) {
-    return new Column(
+    return new Stack(
       children: <Widget>[
-        new Container(
-          height: 180.0,
-          child: new Stack(
-            children: <Widget>[
-              new Positioned(
-                top: 0.0,
-                left: 0.0,
-                right: 0.0,
-                child: new Container(
-                  color: Colors.red,
-                  height: 45.0,
+        new Positioned(
+          bottom: 0.0,
+          left: 0.0,
+          right: 0.0,
+          child: new Container(
+            decoration: new BoxDecoration(
+                color: Colors.white,
+                border: new Border(
+                    top: new BorderSide(width: 0.5, color: Colors.black12))),
+            height: 50.0,
+            child: new Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                new Container(
+                  width: 100.0,
+                  margin: new EdgeInsets.symmetric(horizontal: 10.0),
+                  child: new MaterialButton(
+                    color: Colors.red,
+                    onPressed: () => {},
+                    child: new Text(
+                      "阅读",
+                      style: Theme.of(context).primaryTextTheme.button,
+                    ),
+                  ),
                 ),
-              ),
-              new Positioned(
-                  top: 0.0,
-                  left: 14.0,
-                  child: new Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      new Card(
-                        elevation: 4.0,
-                        child: new Hero(
-                          tag: widget.comic["title"],
-                          child: new GestureDetector(
-                            onScaleStart: _handleOnScaleStart,
-                            onScaleUpdate: _handleOnScaleUpdate,
-                            onScaleEnd: _handleOnScaleEnd,
-                            child: new ClipRect(
-                              child: new Transform(
-                                transform: new Matrix4.identity()
-                                  ..translate(_offset.dx, _offset.dy)
-                                  ..scale(_scale),
-                                child: new Image.network(
-                                  widget.comic['cover'],
-                                  width: 110.0,
-                                  height: 150.0,
-                                  headers: imageHeader,
-                                  fit: BoxFit.cover,
+              ],
+            ),
+          ),
+        ),
+        new Positioned.fill(
+          child: new ListView(
+            children: <Widget>[
+              new Container(
+                height: 180.0,
+                child: new Stack(
+                  children: <Widget>[
+                    new Positioned(
+                      top: 0.0,
+                      left: 0.0,
+                      right: 0.0,
+                      child: new Container(
+                        color: Colors.red,
+                        height: 35.0,
+                      ),
+                    ),
+                    new Positioned(
+                        top: 0.0,
+                        left: 14.0,
+                        child: new Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            new Card(
+                              elevation: 4.0,
+                              child: new Hero(
+                                tag: widget.comic["title"],
+                                child: new GestureDetector(
+                                  onScaleStart: _handleOnScaleStart,
+                                  onScaleUpdate: _handleOnScaleUpdate,
+                                  onScaleEnd: _handleOnScaleEnd,
+                                  child: new ClipRect(
+                                    child: new Transform(
+                                      transform: new Matrix4.identity()
+                                        ..translate(_offset.dx, _offset.dy)
+                                        ..scale(_scale),
+                                      child: new Image.network(
+                                        widget.comic['cover'],
+                                        width: 110.0,
+                                        height: 150.0,
+                                        headers: imageHeader,
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                        ),
-                      ),
-                      new Padding(
-                        padding: new EdgeInsets.symmetric(horizontal: 8.0),
-                      ),
-                      new Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          new Padding(
-                            padding: new EdgeInsets.symmetric(vertical: 28.0),
-                          ),
-                          new Text(
-                            widget.comic["title"],
-                            style: Theme.of(context).textTheme.title,
-                          ),
-                          new Padding(
-                            padding: new EdgeInsets.symmetric(vertical: 3.0),
-                          ),
-                          new Text(
-                            widget.comic['authors'],
-                            style: new TextStyle(
-                                color: Colors.red, fontSize: 16.0),
-                          ),
-                          new Padding(
-                            padding: new EdgeInsets.symmetric(vertical: 3.0),
-                          ),
-                          new Text("战斗力: ${widget.comic['hot_hits']}"),
-                        ],
-                      )
-                    ],
-                  )),
-              new Positioned(
-                  right: 16.0,
-                  top: 25.0,
-                  child: new Text(
-                    "${formatDate(_detailData['last_updatetime'])}更新",
-                    style: Theme.of(context).primaryTextTheme.caption,
-                  )),
+                            new Padding(
+                              padding:
+                                  new EdgeInsets.symmetric(horizontal: 8.0),
+                            ),
+                            new Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                new Text(
+                                  widget.comic["title"],
+                                  style: Theme.of(context).textTheme.title,
+                                ),
+                                new Padding(
+                                  padding:
+                                      new EdgeInsets.symmetric(vertical: 3.0),
+                                ),
+                                new Text(
+                                  widget.comic['authors'],
+                                  style: new TextStyle(
+                                      color: Colors.red, fontSize: 16.0),
+                                ),
+                                new Padding(
+                                  padding:
+                                      new EdgeInsets.symmetric(vertical: 3.0),
+                                ),
+                                new Text("战斗力: ${widget.comic['hot_hits']}"),
+                              ],
+                            )
+                          ],
+                        )),
+                    new Positioned(
+                        right: 16.0,
+                        top: 16.0,
+                        child: new Text(
+                          "${formatDate(_detailData['last_updatetime'])}更新",
+                          style: Theme.of(context).primaryTextTheme.caption,
+                        )),
+                  ],
+                ),
+              ),
+              new Container(
+                margin: new EdgeInsets.symmetric(horizontal: 14.0),
+                child: new Text(_detailData['description'] ?? ''),
+              ),
+              buildChapters(context),
             ],
           ),
-        ),
-        new Container(
-          margin: new EdgeInsets.symmetric(horizontal: 14.0),
-          child: new Text(_detailData['description']??''),
         ),
       ],
     );
   }
 
+  Widget buildChapters(BuildContext context) {
+    if (_detailData.containsKey("chapters"))
+      return new Column(
+          children: (_detailData['chapters'] as List<Map>).map((chapter) {
+        return new Column(
+          children: <Widget>[
+            new Text(chapter['title']),
+            new Flex(
+              direction: Axis.horizontal,
+              children: (chapter['data'] as List).map((data){
+
+              }).toList(),
+            )
+          ],
+        );
+      }).toList());
+    else
+      return new CupertinoActivityIndicator();
+  }
+  Widget buildChapterItem(Map data) {
+      return new Column(
+          children: (_detailData['chapters'] as List<Map>).map((chapter) {
+            return new Column(
+              children: <Widget>[
+                new Text(chapter['title']),
+                new Flex(
+                  direction: Axis.horizontal,
+                  children: (chapter['data'] as List).map((data){
+
+                  }).toList(),
+                )
+              ],
+            );
+          }).toList());
+  }
   String formatDate(int long) {
     if (long == null) return '';
     var now = new DateTime.fromMillisecondsSinceEpoch(long * 1000);
